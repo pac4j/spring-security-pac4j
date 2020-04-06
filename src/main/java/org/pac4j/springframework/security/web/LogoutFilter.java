5fee6243a11a8ba372c605f04e47d9ae7947502a
Jérôme LELEU <leleuj@gmail.com>
2020-04-06 15:29:42 +0200

    first commit

--- src/main/java/org/pac4j/springframework/security/web/LogoutFilter.java
+++ src/main/java/org/pac4j/springframework/security/web/LogoutFilter.java
@@ -69,6 +69,9 @@ public class LogoutFilter extends AbstractPathFilter {
     @Override
     public void destroy() { }
 
+    @Override
+    protected void internalInit() { }
+
     public LogoutLogic<Object, JEEContext> getLogoutLogic() {
         return logoutLogic;
     }
