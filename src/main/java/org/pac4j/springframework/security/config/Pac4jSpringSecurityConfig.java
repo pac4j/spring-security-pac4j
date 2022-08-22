ffcea503af5fcc4872d422257c9e96771f93ba0e
Jérôme LELEU <leleuj@gmail.com>
2022-08-22 14:34:36 +0200

    fix bridge config

--- src/main/java/org/pac4j/springframework/security/config/Pac4jSpringSecurityConfig.java
+++ src/main/java/org/pac4j/springframework/security/config/Pac4jSpringSecurityConfig.java
@@ -4,7 +4,10 @@ import org.pac4j.core.config.Config;
 import org.pac4j.springframework.security.profile.SpringSecurityProfileManager;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+
+import javax.annotation.PostConstruct;
 
 /**
  * Configure the bridge from pac4j to Spring Security.
@@ -17,7 +20,11 @@ public class Pac4jSpringSecurityConfig {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(Pac4jSpringSecurityConfig.class);
 
-    public void setConfig(final Config config) {
+    @Autowired
+    private Config config;
+
+    @PostConstruct
+    public void postConstruct() {
         LOGGER.info("Initializing pac4j to Spring Security bridge...");
 
         config.defaultProfileManagerFactory((ctx, session) -> new SpringSecurityProfileManager(ctx, session));
